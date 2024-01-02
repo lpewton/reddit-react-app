89576ded2b364415d01e4e866062e7bec8c32322
Liam Pewton <liampewton@Liams-MacBook-Air.local>
2024-01-02 23:57:33 +0000

    Link reddit API with app

--- src/features/home/home.js
+++ src/features/home/home.js
@@ -1,43 +1,45 @@
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
-import Article from './article'
-
 export default function Home() {
 
-    const [data, setData] = useState(null)
+    const [data, setData] = useState()
 
     // Get Articles from false API
     const getArticles = async () => {
-        const requestUrl = '../../mocks/data/articles.json'
+        const requestUrl = `https://www.reddit.com/r/archaeology/new.json`;
+
         const response = await fetch(requestUrl, {
             method: 'GET'
         })
         const json = await response.json();
-        return json;
+        return json.data.children;
     }
-    
+
     // Put articles into Data state
     useEffect(() => {
         async function getArticlesData() {
             const articlesData = await getArticles();
-            setData(articlesData);
+            let filteredData = articlesData.filter(article => article.data.thumbnail.includes('http'))
+            setData(filteredData);
         }
-        
+
         getArticlesData();
-        console.log('Hi')
-    }, );
-/*
+    }, []);
+
     if (!data) {
         return <h2>Loading...</h2>;
-      }*/
+    }
+    console.log(data)
 
     return (
         <div>
-            <Article />
-            <Article />
-            <Article />
-            <Article />
+            {data.map((article) => (
+                <div className="cardDiv">
+                <h4>{article.data.title}</h4>
+                <img src={article.data.thumbnail} alt="Article image"></img>
+            </div>
+            ))}
         </div>
     )
 }
