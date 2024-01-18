64b4770a65a83f0ef2ad53c29d0e914fdeaf0a48
Liam Pewton <liampewton@Liams-MacBook-Air.local>
2024-01-18 02:08:09 +0000

    Add function to remove article from LocalStorage

--- src/features/favorites/favorites.js
+++ src/features/favorites/favorites.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Notes from "../notes/notes";
+import { removeFavorite } from "./favoritesSlice";
 
 
 
@@ -9,24 +10,39 @@ export default function Favorites() {
     const [favoriteArticles, setFavoriteArticles] = useState([]);
 
     const dispatch = useDispatch();
-    
+
     const onRemoveArticleHandler = (article) => {
+        const existingDataString = localStorage.getItem('1');
+        dispatch(removeFavorite(article))
+
+            // Retrieve existing data from local storage
+            let existingData = JSON.parse(existingDataString);
+
+            // Ensure existingData is an array
+            existingData = Array.isArray(existingData) ? existingData : [];
+
+            // Filter out the article to remove
+            const updatedData = existingData.filter((existingArticle) => existingArticle.data.title !== article.data.title);
+
+            // Set the updated data back to local storage
+            localStorage.setItem('1', JSON.stringify(updatedData)); 
+            setFavoriteArticles(updatedData);
     };
 
     useEffect(() => {
         // Step 1: Retrieve data from localStorage
         const storedArticlesString = localStorage.getItem('1');
-    
+
         if (storedArticlesString) {
-          // Step 2: Parse the data
-          const parsedData = JSON.parse(storedArticlesString);
-          
-          // Step 3: Set the parsed data to the state
-          for (const article of parsedData) {
-            setFavoriteArticles((prevArticles) => [...prevArticles, article]);
+            // Step 2: Parse the data
+            const parsedData = JSON.parse(storedArticlesString);
+
+            // Step 3: Set the parsed data to the state
+            for (const article of parsedData) {
+                setFavoriteArticles((prevArticles) => [...prevArticles, article]);
+            }
         }
-        }
-      }, []);
+    },[]);
 
     return (
         <div>
@@ -44,7 +60,7 @@ export default function Favorites() {
                     <Link to={article.data.url} target="_blank">
                         <img src={article.data.thumbnail} alt="Article image"></img>
                     </Link>
-                    <Notes id={article.data.id}/>
+                    <Notes id={article.data.id} />
                 </div>
             ))}
         </div>
